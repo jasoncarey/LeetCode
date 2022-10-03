9662b83e94aebcfee554fb632bd8dd96a89f8c5c
Jason Carey <jasdcarey@gmail.com>
2022-10-03 15:34:33 +1100

    53. Maximum Subarray

---
+++ 53-maximum-subarray.py
@@ -0,0 +1,16 @@
+# Maximum Subarray
+# ----------
+# Medium
+# Greedy, Kadanes
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        max_sum = nums[0]
+        curr_sum = nums[0]
+
+        for num in nums[1:]:
+            curr_sum = max( num, curr_sum + num)
+            max_sum = max( curr_sum, max_sum )
+        
+        return max_sum
