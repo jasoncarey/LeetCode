a34f0793022da6d3e69411cd29ce98ac5ab26962
Jason Carey <jasdcarey@gmail.com>
2022-10-03 15:42:27 +1100

    Add dp solution to 53

--- 53-maximum-subarray.py
+++ 53-maximum-subarray.py
@@ -1,8 +1,20 @@
 # Maximum Subarray
 # ----------
 # Medium
-# Greedy, Kadanes
+# Dynamics Programming, Kadanes Algorithm, Greedy
 
+# Dynamic Programming Solution:
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        dp = [0]*len(nums)
+
+        for i, num in enumerate(nums):
+            dp[i] = max( dp[i-1] + num, num)
+
+        return max(dp)
+
+# Kadane's Solution:
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
